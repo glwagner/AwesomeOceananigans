ec8b76de3b6fe5656066d180810ce270f00359eb
Gregory Wagner <wagner.greg@gmail.com>
2024-03-14 09:43:23 -0400

    Update setup

--- BaroclinicAdjustments/hydrostatic_submesoscale_baroclinic_adjustment.jl
+++ BaroclinicAdjustments/hydrostatic_submesoscale_baroclinic_adjustment.jl
@@ -8,19 +8,20 @@
 
 using Oceananigans
 using Oceananigans.Units
-using Oceananigans.TurbulenceClosures: CATKEVerticalDiffusivity
+using Oceananigans.TurbulenceClosures: CATKEVerticalDiffusivity, RiBasedVerticalDiffusivity
+using Oceananigans.TurbulenceClosures: FivePointHorizontalFilter
 
 arch = GPU()
-Nx = 256 
-Ny = 256
-Nz = 96
+Nx = Ny = 256 
+Nz = 80
+weno_order = 5
 save_fields_interval = 1hour
 output_directory = "." #"nobackup/users/glwagner/awesome-oceananigans" #"."
-filename = "hydrostatic_submesoscale_baroclinic_adjustment_Nx$(Nx)_Nz$(Nz)"
 
 include("submesoscale_baroclinic_adjustment_setup.jl")
 
-setup = langmuir_density_front_setup()
+setup = langmuir_density_front_setup() #u★=0.0)
+Lx, Ly, Lz = setup.domain
 
 grid = RectilinearGrid(arch,
                        size = (Nx, Ny, Nz),
@@ -30,59 +31,80 @@ grid = RectilinearGrid(arch,
                        z = (-Lz, 0),
                        topology = (Periodic, Bounded, Bounded))
 
-model = HydrostaticFreeSurfaceModel(; grid,
-                                    closure = CATKEVerticalDiffusivity(),
-                                    boundary_conditions = setup.boundary_conditions,
-                                    coriolis = setup.coriolis,
-                                    buoyancy = setup.buoyancy,
-                                    tracers = (setup.tracers..., :e),
-                                    momentum_advection = WENO(order=9),
-                                    tracer_advection = WENO(order=9))
+closures = [
+    CATKEVerticalDiffusivity()
+    RiBasedVerticalDiffusivity()
+#    RiBasedVerticalDiffusivity(horizontal_Ri_filter=FivePointHorizontalFilter())
+    ConvectiveAdjustmentVerticalDiffusivity(convective_κz=1e-1, convective_νz=1e-2)
+]
 
-set!(model; e=1e-6, setup.initial_condition...)
+for closure in closures
 
-simulation = Simulation(model, Δt=1minute, stop_time=30days)
-conjure_time_step_wizard!(simulation, IterationInterval(20), cfl=0.3, max_Δt=20minutes)
-b = model.tracers.b
-e = model.tracers.e
-u, v, w = model.velocities
-ζ = ∂x(v) - ∂y(u)
+    prefix = if closure isa CATKEVerticalDiffusivity
+                "catke"
+             elseif closure isa RiBasedVerticalDiffusivity
+                "ri_based"
+             elseif closure isa ConvectiveAdjustmentVerticalDiffusivity
+                "convective_adjustment"
+             end
 
-wall_clock = Ref(time_ns())
+    filename = "$(prefix)_modified_hydrostatic_submesoscale_baroclinic_adjustment_Nx$(Nx)_Nz$(Nz)"
 
-function print_progress(sim)
+    @show closure
+
+    model = HydrostaticFreeSurfaceModel(; grid, closure,
+                                        boundary_conditions = setup.boundary_conditions,
+                                        coriolis = setup.coriolis,
+                                        buoyancy = setup.buoyancy,
+                                        tracers = (setup.tracers..., :e),
+                                        momentum_advection = WENO(order=weno_order),
+                                        tracer_advection = WENO(order=weno_order))
+
+    set!(model; e=1e-6, setup.initial_condition...)
+
+    simulation = Simulation(model, Δt=1minute, stop_time=30days)
+    conjure_time_step_wizard!(simulation, IterationInterval(20), cfl=0.3, max_Δt=20minutes)
+    b = model.tracers.b
+    e = model.tracers.e
     u, v, w = model.velocities
-    progress = 100 * (time(sim) / sim.stop_time)
-    elapsed = (time_ns() - wall_clock[]) / 1e9
+    ζ = ∂x(v) - ∂y(u)
 
-    @printf("[%05.2f%%] i: %d, t: %s, wall time: %s, max(u): (%6.3e, %6.3e, %6.3e) m/s, next Δt: %s\n",
-            progress, iteration(sim), prettytime(sim), prettytime(elapsed),
-            maximum(abs, interior(u)),
-            maximum(abs, interior(v)),
-            maximum(abs, interior(w)),
-            prettytime(sim.Δt))
+    wall_clock = Ref(time_ns())
 
-    wall_clock[] = time_ns()
-    
-    return nothing
+    function print_progress(sim)
+        u, v, w = model.velocities
+        progress = 100 * (time(sim) / sim.stop_time)
+        elapsed = (time_ns() - wall_clock[]) / 1e9
+
+        @printf("[%05.2f%%] i: %d, t: %s, wall time: %s, max(u): (%6.3e, %6.3e, %6.3e) m/s, next Δt: %s\n",
+                progress, iteration(sim), prettytime(sim), prettytime(elapsed),
+                maximum(abs, interior(u)),
+                maximum(abs, interior(v)),
+                maximum(abs, interior(w)),
+                prettytime(sim.Δt))
+
+        wall_clock[] = time_ns()
+        
+        return nothing
+    end
+
+    add_callback!(simulation, print_progress, IterationInterval(100))
+
+    slicers = (east = (grid.Nx, :, :),
+               north = (:, grid.Ny, :),
+               top = (:, :, grid.Nz))
+
+    for side in keys(slicers)
+        indices = slicers[side]
+
+        simulation.output_writers[side] = JLD2OutputWriter(model, (; b, ζ, e);
+                                                           filename = filename * "_$(side)_slice",
+                                                           dir = output_directory,
+                                                           schedule = TimeInterval(save_fields_interval),
+                                                           overwrite_existing = true,
+                                                           indices)
+    end
+
+    run!(simulation)
+
 end
-
-add_callback!(simulation, print_progress, IterationInterval(100))
-
-slicers = (east = (grid.Nx, :, :),
-           north = (:, grid.Ny, :),
-           top = (:, :, grid.Nz))
-
-for side in keys(slicers)
-    indices = slicers[side]
-
-    simulation.output_writers[side] = JLD2OutputWriter(model, (; b, ζ, e);
-                                                       filename = filename * "_$(side)_slice",
-                                                       dir = output_directory,
-                                                       schedule = TimeInterval(save_fields_interval),
-                                                       overwrite_existing = true,
-                                                       indices)
-end
-
-run!(simulation)
-
